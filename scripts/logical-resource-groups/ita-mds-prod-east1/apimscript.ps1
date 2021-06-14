6748a3f78525a603580990bfa6d78732039a380e
gtarpehh <george.tarpeh@trade.gov>
2021-06-14 12:49:14 -0400

    Updated ita-mds-prod-east1 scripts

---
+++ scripts/logical-resource-groups/ita-mds-prod-east1/apimscript.ps1
@@ -0,0 +1,27 @@
+﻿#connect to your account and specify the subscription to deploy to
+Connect-AzAccount -Subscription <subscription>
+
+#create a new resources group and specify the location
+New-AzResourceGroup `
+  -Name ita-mds-prod-east1 `
+  -Location "East US"
+
+#set your RG & apim name and other details 
+$RGName ="ita-mds-prod-east1"
+$ApimServiceName = "apim-mds-prod"
+$Location = "East US"
+$Organisation = "<organizationname>"
+$AdminEmail = "<adminemail>"
+
+
+#this command will create azure api manager
+New-AzApiManagement -ResourceGroupName $RGName -Name $ApimServiceName -Location $Location -Organization $Organisation -AdminEmail $AdminEmail
+
+#this will deploy ARM template
+New-AzResourceGroupDeployment `
+-Name MyARMDeployment `
+-ResourceGroupName $RGName `
+-TemplateUri <PathOrLinkToTemplateUri> `
+-TemplateParameterUri <PathOrLinkToParameterUri>
+
+
