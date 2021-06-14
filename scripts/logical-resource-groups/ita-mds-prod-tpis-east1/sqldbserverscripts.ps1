cb46d679157482a5e92ed2b0fdb85843db48c8c7
gtarpehh <george.tarpeh@trade.gov>
2021-06-14 14:37:58 -0400

    Updated

---
+++ scripts/logical-resource-groups/ita-mds-prod-tpis-east1/sqldbserverscripts.ps1
@@ -0,0 +1,22 @@
+﻿#connect to your account and specify the subscription to deploy to
+Connect-AzAccount -Subscription <subscription>
+
+#create a new resources group if it doesnt exist and specify the location
+New-AzResourceGroup `
+  -Name ITA-MDS-TPIS-Prod-East1  `
+  -Location "East US"
+
+#set your RG & sql server name
+$RGName ="ITA-MDS-TPIS-Prod-East1 "
+$ServerName = "ita-mds-tpisserver-prod-east1"
+$Location = "East US"
+
+
+#this will deploy ARM template
+New-AzResourceGroupDeployment `
+-Name MyARMDeployment `
+-ResourceGroupName $RGName `
+-TemplateUri <PathOrLinkToTemplateUri> `
+-TemplateParameterUri <PathOrLinkToParameterUri>
+
+
