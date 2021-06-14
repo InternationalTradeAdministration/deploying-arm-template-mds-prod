ebc4a3b714b4d439dbb1c4449b3a005285a0c8bd
gtarpehh <george.tarpeh@trade.gov>
2021-06-14 13:25:55 -0400

    Added script to -ta-mds-prod-tpis-east1

---
+++ scripts/logical-resource-groups/ita-mds-prod-tpis-east1/adfscript.ps1
@@ -0,0 +1,21 @@
+﻿#connect to your account and specify the subscription to deploy to
+Connect-AzAccount -Subscription <subscription>
+
+#create a new resources group and specify the location
+New-AzResourceGroup `
+  -Name ita-mds-prod-tpis-east1 `
+  -Location "East US"
+
+#set your RG & data factory name
+$resourceGroupName ="ita-mds-prod-tpis-east1"
+$dataFactoryName = "adf-mdstpis-Prod"
+
+
+#this will deploy ARM template
+New-AzResourceGroupDeployment `
+-Name MyARMDeployment `
+-ResourceGroupName $resourceGroupName `
+-TemplateUri <PathOrLinkToTemplateUri> `
+-TemplateParameterUri <PathOrLinkToParameterUri>
+
+
