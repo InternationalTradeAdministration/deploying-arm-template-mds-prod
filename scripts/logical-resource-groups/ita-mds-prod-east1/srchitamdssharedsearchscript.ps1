569a9104e8464c69a64740f7e4e251b0cbed32f4
gtarpehh <george.tarpeh@trade.gov>
2021-08-09 13:31:24 -0400

    Updated Scripts

---
+++ scripts/logical-resource-groups/ita-mds-prod-east1/srchitamdssharedsearchscript.ps1
@@ -0,0 +1,20 @@
+﻿#connect to your account 
+Connect-AzAccount -Subscription <subscriptionid>
+
+#create a new resources group if it doesnt exist and specify the location
+New-AzResourceGroup `
+  -Name ITA-MDS-Prod-East1 `
+  -Location "East US"
+
+#set your Cognitive Search & RG name
+$RGName ="ITA-MDS-Prod-East1"
+$VaultName = "ita-mds-shared-search-prod"
+$Location = "East US"
+
+
+#this will deploy ARM template
+New-AzResourceGroupDeployment `
+-Name MyARMDeployment `
+-ResourceGroupName $RGName `
+-TemplateUri <PathOrLinkToTemplateUri> `
+-TemplateParameterUri <PathOrLinkToParameterUri>
